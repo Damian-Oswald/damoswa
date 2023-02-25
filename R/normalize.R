986883a0229dd62e415723a7ff0aea773b401be4
Answaltan <61975699+Answaltan@users.noreply.github.com>
2023-02-24 17:25:52 -0800

    Changes in various functions, for example how the R-squared is computed, or how the normalization is performed. Normalization allows to set desired minimum and maximum.

--- R/normalize.R
+++ R/normalize.R
@@ -3,6 +3,6 @@
 #' This function normalizes an input vector `x`, i.e. it re-scales the vector between 0 and 1.
 #' 
 #' @export
-normalize <- function(x, na.rm = TRUE) {
-   (x - min(x, na.rm = na.rm)) / (max(x, na.rm = na.rm) - min(x, na.rm = na.rm))
+normalize <- function(x, na.rm = TRUE, minimum = 0, maximum = 1) {
+   (x - min(x, na.rm = na.rm)) / (max(x, na.rm = na.rm) - min(x, na.rm = na.rm))*maximum + minimum
 }
